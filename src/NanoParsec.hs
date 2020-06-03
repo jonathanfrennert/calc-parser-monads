b1c5a63d384c035ff80b1ebe8bc38d334e860b1b
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2020-06-03 10:26:52 +0200

    Add Parser failure and rollover

--- src/NanoParsec.hs
+++ src/NanoParsec.hs
@@ -34,29 +34,67 @@ item = Parser $ \s ->
 
 ------------------------------ Parser Monad ------------------------------
 
--- | The parser monad bind operation. Make a composition of two parser operations.
-bind :: Parser a          -- ^ Parser with the first operation
-     -> (a -> Parser b)   -- ^ Parser with second operation
-     -> Parser b
-bind fstP sndP = Parser $ \s -> concatMap (\(a, s') -> parse (sndP a) s') $ parse fstP s
-
--- | The parser monad identity operation. Inject a single pure value as the result.
+-- | Inject a single pure value as the result.
 unit :: a           -- ^ Pure value
      -> Parser a
 unit a = Parser (\s -> [(a,s)])
 
--- | The functor parser applies a function to the first element of every tuple
--- resulting from its parse operation.
-instance Functor Parser where
-  fmap f (Parser cs) = Parser (\s -> [(f a, b) | (a, b) <- cs s])
+-- | Make a composition of two parser operations.
+bind :: Parser a          -- ^ Parser with the first operation
+     -> (a -> Parser b)   -- ^ Parser with second operation
+     -> Parser b
+bind p q = Parser $ \s -> concatMap (\(a, s') -> parse (q a) s') $ parse p s
 
--- | TODO
-instance Applicative Parser where
-  pure                          = return
-  (Parser cs1) <*> (Parser cs2) = Parser (\s1 -> [(f a, s3) | (f, s2) <- cs1 s1,
-                                                              (a, s3) <- cs2 s2])
+-- | Create an empty list.
+failure :: Parser a
+failure = Parser (\s -> [])
 
--- | TODO
+-- | Choose between two parser operations, switching to the second operation if
+-- the first operation results in a 'failure'.
+option :: Parser a    -- ^ Parser with the first operation
+       -> Parser a    -- ^ Parser with the second operation
+       -> Parser a
+option  p q = Parser $ \s ->
+  case parse p s of
+    []  -> parse q s
+    res -> res
+
+-- | Apply two parser operations over the same stream and concatenate the result.
+combine :: Parser a   -- ^ Parser whose operation is first in the result
+        -> Parser a   -- ^ Parser whose operation is second in the result
+        -> Parser a
+combine p q = Parser (\s -> parse p s ++ parse q s)
+
+-- |
+-- * The identity operation is 'unit'.
+-- * The binary associative binary operation is 'bind'.
 instance Monad Parser where
   return = unit
   (>>=)  = bind
+
+-- | Apply a function to the first element of every tuple resulting from the parse operation.
+instance Functor Parser where
+  fmap f p = p >>= return . f
+
+-- |
+-- * The identity operation is 'return'.
+-- * The binary associative operation applies the LHS parser operation to the
+-- the RHS parser operation and retains only the RHS stream.
+instance Applicative Parser where
+  pure                          = return
+  (Parser op1) <*> (Parser op2) = Parser (\s1 -> [(f a, s3) | (f, s2) <- op1 s1,
+                                                              (a, s3) <- op2 s2])
+
+-- |
+-- * The identity operation is 'failure'.
+-- * The binary associative operation is 'option'.
+instance Alternative Parser where
+  empty = failure
+  (<|>) = option
+
+-- |
+-- * The identity operation is 'empty'
+-- * The binary associative operation is 'combine'
+instance MonadPlus Parser where
+  mzero = empty
+  mplus = combine
